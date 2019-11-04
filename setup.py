c94145f9647149fefe39fd915f32b82f156d55f5
sabuj <md.sabuj.sarker@gmail.com>
2019-11-04 20:52:01 +0600

    setup.py added, converted readme  md2rst added

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='locallibs',
+    version='0.1.0.dev1',
+    description='Local Library Management Tool',
+    author='Md. Sabuj Sarker',
+    author_email='md.sabuj.sarker@gmail.com',
+    url='https://github.com/SabujXi/locallibs',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'locallibs = locallibs:locallibs_cli'
+        ]
+    },
+    data_files=[('', ['README.rst', 'LICENSE'])],
+    package_data={
+        '': ['*.txt', '*.rst', '*.md'],
+    },
+    python_requires='>=2.7',
+    long_description=long_description
+)
